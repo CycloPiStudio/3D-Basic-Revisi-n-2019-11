2d08818e1e1b460a9534452e8111c81a50f75898
CycloPi <venti3d@gmail.com>
2019-11-02 01:32:11 +0100

    subir  de nivel en el nivel

--- Main/Ejecutable/Juego/Escenarios/Niveles/Nivel1/Nivel1.gd
+++ Main/Ejecutable/Juego/Escenarios/Niveles/Nivel1/Nivel1.gd
@@ -1,6 +1,7 @@
 extends Node2D
 #
-var preMenu = load("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn")
+#var preMenu = load("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn")
+var preMenu = load("res://Ejecutable/Menus/Select Niveles/SelectNiveles.tscn")
 var Menu
 var My_timer = 0
 var segundos = 3
@@ -13,8 +14,12 @@ func _on_Timer_timeout():
 	My_timer += 1
 	get_node("Label").set_text(str(segundos-My_timer))
 	if My_timer > segundos:
+		Global.subirnivel()
+		
 		Menu =preMenu.instance()
 		get_node("/root/Global Menus").add_child(Menu)
+		
+#		Global.nivel = Global.nivel+1
 #		get_node("/root/Nodo_Dios/Music_menu").play()
 		$".".queue_free()
 	get_node("Timer").start()
