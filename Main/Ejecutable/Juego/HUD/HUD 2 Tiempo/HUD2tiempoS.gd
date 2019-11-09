b9e14ef621f6c1a5d336f002f6edd44c9d3fb0a7
CycloPi <venti3d@gmail.com>
2019-11-09 20:06:05 +0100

    Add files via upload

--- Main/Ejecutable/Juego/HUD/HUD 2 Tiempo/HUD2tiempoS.gd
+++ Main/Ejecutable/Juego/HUD/HUD 2 Tiempo/HUD2tiempoS.gd
@@ -4,7 +4,7 @@ onready var start_time = OS.get_ticks_msec()
 onready var label = $TextureRect/HBoxContainer/Tiempo
 
 
-func _process(delta):
+func _process(_delta):
 
 	label.set_text(str((OS.get_ticks_msec() - start_time)/1000))
 	
