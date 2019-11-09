5e198e40dc451ecfbd95219675b16d41823885d7
CycloPi <venti3d@gmail.com>
2019-11-09 19:43:32 +0100

    Actualizando la pantalla 2

--- Main/Ejecutable/Juego/Escenarios/Niveles/Nivel2/nivel2.gd
+++ Main/Ejecutable/Juego/Escenarios/Niveles/Nivel2/nivel2.gd
@@ -1,26 +1,26 @@
 #	esto hay cambiarlo o quitarlo
 extends Node2D
-#var cancion
-#
-#onready var Fondo = $Fondo
-#
-#func _ready():
-#	centrar_Fondo()
-#	cancion = get_node("AudioMusicNivel_01")
-#	cancion.play()
-#
-#	pass
-#
-#func _process(delta):
-##	print (Position2D.)
-##	print ("position: " + str(position))
-##	print ("global position: " + str(global_position))
-#	if cancion.is_playing():
-#		pass
-#	else:
-#		cancion.play()
-#
-#
-#func centrar_Fondo():
-#	#set_position(get_viewport().size/2)
-#	pass
+var cancion
+
+onready var Fondo = $Fondo
+
+func _ready():
+	centrar_Fondo()
+	cancion = get_node("AudioMusicNivel_01")
+	cancion.play()
+	
+	pass
+
+func _process(delta):
+#	print (Position2D.)
+#	print ("position: " + str(position))
+#	print ("global position: " + str(global_position))
+	if cancion.is_playing():
+		pass
+	else:
+		cancion.play()
+		
+
+func centrar_Fondo():
+	#set_position(get_viewport().size/2)
+	pass
