484e42a0a98e59a0e304e70fc41c4789283b7a50
juanantoniogit <juanantonio.tecno@gmail.com>
2019-11-12 12:47:33 +0100

    independizo el temporizador de los niveles

---
+++ Main/Ejecutable/Juego/Escenarios/Elementos/Temporizador/Temporizador.gd
@@ -0,0 +1,34 @@
+extends Node2D
+#
+#var preMenu = load("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn")
+var preMenu = load("res://Ejecutable/Menus/Select Niveles/SelectNiveles.tscn")
+var Menu
+var My_timer = 0
+var segundos = 3
+
+func _ready():
+	get_node("Label").set_text(str(segundos-My_timer))
+#
+func _on_Timer_timeout():
+
+	My_timer += 1
+	get_node("Label").set_text(str(segundos-My_timer))
+	if My_timer > segundos:
+		print(get_parent().name)
+		if get_parent().name == "Global Menus":
+			Global.subirnivel()
+			Menu =preMenu.instance()
+			get_node("/root/Global Menus").add_child(Menu)
+	#		Global.nivel = Global.nivel+1
+	#		get_node("/root/Nodo_Dios/Music_menu").play()
+			
+			$".".queue_free()
+		else:
+			Global.subirnivel()
+			Menu =preMenu.instance()
+			get_node("/root/Global Menus").add_child(Menu)
+#			Global.personaje.get_path()
+			get_parent().queue_free()
+			
+	get_node("Timer").start()
+	pass # Replace with function body.
